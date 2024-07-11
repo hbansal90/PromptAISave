394f10bbd09e90ecd40fa214810bc232bbf6a252
Harsh Bansal <hbnsl890@gmail.com>
2024-07-12 00:55:31 +0530

    Rebasing Git for .env file

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <span className="orange_gradient text-center"> AI-powered Prompts</span>
       </h1>
       <p className="desc text-center">
-        PromptSaveAI is an open-source tool to Save, Discover, and Share your AI LNM models generated creative prompts.
+        PromptSaveAI is an open-source tool to Save, Discover, and Share your AI LLM models generated creative prompts.
       </p>
       <Feed/>
     </section>
