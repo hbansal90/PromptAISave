b608b4f8edd8e95da2f17e6fc5e10755374d9255
Harsh Bansal <hbnsl890@gmail.com>
2024-07-18 15:26:14 +0530

    Put cache no store

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -10,7 +10,7 @@ const MyProfile = () => {
     const router = useRouter()
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = await fetch(`/api/users/${session?.user.id}/posts`)
+            const response = await fetch(`/api/users/${session?.user.id}/posts`, {cache: 'no-store'})
             const data = await response.json()
             setPosts(data)
         }
